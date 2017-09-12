640c4fe578841532db784fd351a8a18346cf79fd
SergeySheyko <ser.sheyko@gmail.com>
2017-09-12 17:26:22 +0300

    refactoring

--- src/main/java/sqlcmd/MyServlet.java
+++ src/main/java/sqlcmd/MyServlet.java
@@ -1,16 +1,14 @@
-package sqlcmd.controller;
+package sqlcmd;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-/**
- * Created by ser on 11.09.2017.
- */
+
 @WebServlet("/s")
 public class MyServlet extends HttpServlet {
 
@@ -23,9 +21,18 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+//        response.setContentType("text/html");
+//        PrintWriter out = response.getWriter();
+//
+//        out.println("Hello Word");
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
 
-        out.println("Hello Word");
+        String varTextA = "Hello World!";
+        request.setAttribute("textA", varTextA);
+        String varTextB = "It JSP.";
+        request.setAttribute("textB", varTextB);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+        dispatcher.forward(request, response);
     }
 }
